ed82965495dc7477c33770fdb7302fdc2078bc9c
ppg63275-ai <ppg63275@gmail.com>
2025-10-27 18:45:50 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -345,16 +345,9 @@ local function SendBrainrotWebhook(b)
         players = tostring(#Players:GetPlayers()).."/"..tostring(Players.MaxPlayers),
         timestamp = os.time(),
     }
-
-    pcall(function()
-        DoRequest({
-            Url = API_URL,
-            Method = "POST",
-            Headers = { ["Content-Type"] = "application/json" },
-            Body = HttpService:JSONEncode(payload)
-        })
-    end)
-
+    if b.Amount >= 50_000_000 then
+        sendtohighlight(b.Amount, b.Name)
+    end
     pcall(function()
         DoRequest({
             Url = PYTHONANYWHERE_URL,
@@ -367,10 +360,15 @@ local function SendBrainrotWebhook(b)
             })
         })
     end)
+    pcall(function()
+        DoRequest({
+            Url = API_URL,
+            Method = "POST",
+            Headers = { ["Content-Type"] = "application/json" },
+            Body = HttpService:JSONEncode(payload)
+        })
+    end)
 
-    if b.Amount >= 50_000_000 then
-        sendtohighlight(b.Amount, b.Name)
-    end
 end
 
 local function postJSON(path, tbl)
