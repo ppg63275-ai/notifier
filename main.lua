15c065b0f7caaa1f9a53eb61dd3a8734785686d9
bbs <ppg63275@gmail.com>
2025-11-16 22:36:00 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ if not fuckyouxyn then
  error('FUCK YOU NIGGER JOIN STEAL A BRAINROT')
  return
 end
+-- hi
 local hikarusuckingmydih = 'AnimalOverhead'
 local pharaoisanigger = 'BillboardGui'
 local fuckinggayfunction = request or http_request or http and http.request
@@ -103,13 +104,13 @@ for femboy, porn in ipairs(fuckyouxyn:GetDescendants()) do
    local analsex = tonumber(gen4) / 1000000
    -- analsex: gen4 / 1 mil, logic: 1b = 1000, 1m = 1
    if analsex >= 1 and analsex <= 10 then -- 1-10m
-    sendthefuckingrequest('https://discord.com/api/webhooks/1439612935495356556/qgcUWOiFYAB3jW1inDRtPutezszqRDKuOlC3ypU3vSdMz303VZIWxPVk_33LQdlCKE02', name, '<@&1428040722715639892>', ohshitiforgot)
+    sendthefuckingrequest('https://discord.com/api/webhooks/1439612935495356556/qgcUWOiFYAB3jW1inDRtPutezszqRDKuOlC3ypU3vSdMz303VZIWxPVk_33LQdlCKE02', name, '<@&1428040722715639892>', gen)
    elseif analsex >= 10 and analsex <= 50 then -- 10-50m
-    sendthefuckingrequest('https://discord.com/api/webhooks/1439612008944369816/ZU1_KxqOtzY5yVWgCxwPQLBxnWTPobDjKQ8p3yZnZuLSwJDos8TStdqCCnusNo1udJGW', name, '<@&1428040796312965222>', ohshitiforgot)
+    sendthefuckingrequest('https://discord.com/api/webhooks/1439612008944369816/ZU1_KxqOtzY5yVWgCxwPQLBxnWTPobDjKQ8p3yZnZuLSwJDos8TStdqCCnusNo1udJGW', name, '<@&1428040796312965222>', gen)
    elseif analsex >= 50 and analsex <= 100 then -- 50-100m
-    sendthefuckingrequest('https://discord.com/api/webhooks/1439612268819120202/f6a3umNSYbktBG89gX6rHxjQk-leeLrDBmKa62B8GwVKNehcxFrLIvxhGSmJ2c58-Cdu', name, '<@&1428040887715237889>', ohshitiforgot)
+    sendthefuckingrequest('https://discord.com/api/webhooks/1439612268819120202/f6a3umNSYbktBG89gX6rHxjQk-leeLrDBmKa62B8GwVKNehcxFrLIvxhGSmJ2c58-Cdu', name, '<@&1428040887715237889>', gen)
    elseif analsex >= 100 then -- 100m+
-    sendthefuckingrequest('https://discord.com/api/webhooks/1439612545936916561/_ISjAmOOXIPXOhvWNd0hHVtrpaK0JLx6ziKMx9HGV6nh95T7gtGdBk3NQVB_IOTaftVb', name, '<@&1428040962139230268>', ohshitiforgot)
+    sendthefuckingrequest('https://discord.com/api/webhooks/1439612545936916561/_ISjAmOOXIPXOhvWNd0hHVtrpaK0JLx6ziKMx9HGV6nh95T7gtGdBk3NQVB_IOTaftVb', name, '<@&1428040962139230268>', gen)
    end
   end
   task.wait()
